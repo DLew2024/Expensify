8d371a5f5c399b02a56c73afbb28af6d7c95c322
DLewDev <dlew.dev@gmail.com>
2026-01-20 20:49:52 -0600

    adding EMPTY GUID

--- src/utils/DataTypes/Guid.ts
+++ src/utils/DataTypes/Guid.ts
@@ -3,6 +3,8 @@ import { GUID_REGEX } from '../Regex/RegexUtils';
 
 export type Guid = string & { readonly __brand: 'Guid' };
 
+export const EMPTY_GUID = '00000000-0000-0000-0000-000000000000' as Guid;
+
 export const isGuid = (value: unknown): value is Guid =>
 	typeof value === 'string' && GUID_REGEX.test(value);
 
