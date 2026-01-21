1de04010746b68ada37becaf6d9b76db3ecb624e
DLewDev <dlew.dev@gmail.com>
2026-01-20 20:49:03 -0600

    Allow list items to be navigated to the edit page on select

--- src/components/Routes/ExpenseDashboard/components/ExpenseListItem.tsx
+++ src/components/Routes/ExpenseDashboard/components/ExpenseListItem.tsx
@@ -1,23 +1,21 @@
-import { useDispatch } from '../../../../store/hooks';
-import { removeExpense } from '../../../../store/slices/expensesSlice';
+import { Link } from 'react-router';
 import type { Expense } from '../../../../utils/DataTypes/ExpenseTypes';
 import { formatEpochSecondsSafe } from '../../../../utils/Functions/Conversions/DateUtils';
 import { convertPriceToString } from '../../../../utils/Functions/Conversions/StringUtils';
+import { NavigationRoutePaths } from '../../../../utils/Navigation/NavigationRoutePaths';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }: Expense) => {
-	const dispatch = useDispatch();
 	return (
 		<div>
 			<h1>Expense List Item</h1>
-			{description}
+			<Link to={NavigationRoutePaths.EDIT_PATTERN(id)}>
+				{<h3 style={{ margin: 0 }}>{description}</h3>}
+			</Link>
 			<br />
 			<p>
 				{`${convertPriceToString(amount)} -
                 ${formatEpochSecondsSafe(createdAt)}`}
 			</p>
-			<button type="button" onClick={() => dispatch(removeExpense(id))}>
-				Remove
-			</button>
 		</div>
 	);
 };
