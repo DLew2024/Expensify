99dce820c80bfa9993d5a24b7c7b8e20c6b8cbf4
DLewDev <dlew.dev@gmail.com>
2026-01-24 16:01:31 -0600

    Add Primary button

---
+++ src/components/UI Components/Applied UI/global.d.ts
@@ -0,0 +1,11 @@
+export interface ElementAttributes {
+	className?: string;
+	hidden?: boolean;
+	id?: string;
+	tabIndex?: number;
+}
+
+export interface AppliedAriaAttributes {
+	'arial-label'?: string;
+	'arial-describedby'?: string;
+}
